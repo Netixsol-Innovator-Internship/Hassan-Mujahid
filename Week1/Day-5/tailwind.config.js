0caaf5be917b65bcedf9779ef6360d33c7aec33f
Hassan-Mujahid <hassanmujahid_intern@netixsol.com>
2024-12-19 14:30:06 +0500

    Completed Project#2

--- Week1/Day-5/tailwind.config.js
+++ Week1/Day-5/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         "dark-blue": "hsl(233, 47%, 7%)",
+        "dark-desaturated-blue": "hsl(244, 38%, 16%)",
+        "soft-violet": "hsl(277, 64%, 61%)",
+        grape: "#862e9c",
+        violet: "hsl(277, 54%, 32%)",
       },
     },
   },

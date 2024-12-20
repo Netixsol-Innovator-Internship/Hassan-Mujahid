2dddf06a24f7eebdd7dff6de65b7357297ee6777
Hassan-Mujahid <hassanmujahid_intern@netixsol.com>
2024-12-20 13:06:15 +0500

    Added Dist folder tailwindConfig and modified .gitignore file

---
+++ Week1/Day-6/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "desired-red": "hsl(0, 100%, 74%)",
+        "modified-red": "hsl(0, 97%, 70%)",
+      },
+      backgroundImage: {
+        "intro-desktop": "url('../images/bg-intro-desktop.png')",
+        "intro-mobile": "url('../images/bg-intro-mobile.png')",
+      },
+    },
+  },
+  plugins: [],
+};

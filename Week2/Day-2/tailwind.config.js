8315027120717c5f6c04e071ce8ebb04e991c95d
Hassan-Mujahid <hassanmujahid_intern@netixsol.com>
2024-12-24 19:30:34 +0500

    Completed Styles

--- Week2/Day-2/tailwind.config.js
+++ Week2/Day-2/tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1920px",
+        "4xl": "2560px",
+      },
+      colors: {
+        "desired-light-blue": "hsl(211, 68%, 94%)",
+        "desired-blue": "hsl(219, 85%, 26%)",
+      },
+      fontFamily: {
+        jakarta: ['"Plus Jakarta Sans"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};

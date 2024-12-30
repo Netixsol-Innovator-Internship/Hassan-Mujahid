0d0c68f2d7726ad9684b36425a6b431c5743fdcd
Hassan-Mujahid <hassanmujahid_intern@netixsol.com>
2024-12-30 11:31:34 +0500

    Completed styles

--- Week2/Day-4/tailwind.config.js
+++ Week2/Day-4/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         "desired-light-gray-cyan": "hsl(185, 41%, 84%)",
         "desired-grayish-cyan": "hsl(184, 14%, 56%)",
         "desired-dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "desired-strong-cyan": "hsl(172, 67%, 45%)",
         "desired-dark-cyan": "hsl(183, 100%, 15%)",
       },
     },

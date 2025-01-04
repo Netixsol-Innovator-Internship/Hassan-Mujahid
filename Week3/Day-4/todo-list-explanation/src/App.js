b51727af44151e8217e7a8798f5f53b2af2bbbde
Hassan-Mujahid <hassanmujahid_intern@netixsol.com>
2025-01-04 18:03:33 +0500

    Completed Code

--- Week3/Day-4/todo-list-explanation/src/App.js
+++ Week3/Day-4/todo-list-explanation/src/App.js
@@ -1,16 +1,56 @@
 import React from "react";
-import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useState, useRef, useEffect } from "react";
 import TodoList from "./Components/TodoList";
 export default function App() {
+  const LOCAL_STORAGE_KEY = "todoapp.todos";
+
   const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (todos.length !== 0)
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  };
+
+  const clearCompletedTodos = () => {
+    const completedTodos = todos.filter((todo) => !todo.complete);
+    if (completedTodos) {
+      setTodos(completedTodos);
+    }
+  };
+
+  const checkedTodos = todos.filter((todo) => todo.complete === false);
 
   return (
     <>
-      <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
-      <button>Clear Complete</button>
-      <div>0 left todo</div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={clearCompletedTodos}>Clear Complete</button>
+      <div>{checkedTodos.length} left todo</div>
     </>
   );
 }
--- Week3/Day-4/todo-list-explanation/src/Components/TodoList.js
+++ Week3/Day-4/todo-list-explanation/src/Components/TodoList.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Todo from "./Todo";
 
-export default function TodoList({ todos }) {
-  return <div>{todos.length}</div>;
+export default function TodoList({ todos, toggleTodo }) {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo.name} toggleTodo={toggleTodo} />
+      ))}
+    </div>
+  );
 }
